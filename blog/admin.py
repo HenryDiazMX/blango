817b212366dc3c1a09257e46b8eb6c0f2eca7d03
Henry Diaz <hdiaz@bemira.app>
2025-02-26 22:59:03 -0600

    Restore data

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from blog.models import Tag, Post, Comment, AuthorProfile
+
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
-    list_display = ('slug', 'published_at')
-
-admin.site.register(Tag)
+  prepopulated_fields = {'slug': ('title',)}
+  list_display = ['slug', 'published_at']
 admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
 admin.site.register(Comment)
+admin.site.register(AuthorProfile)
--- blog/forms.py
+++ blog/forms.py
@@ -1,14 +1,14 @@
-from django import forms
+from django.forms import ModelForm
 from blog.models import Comment
-from crispy_forms.layout import Submit
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ["content"]
+class CommentForm(ModelForm):
+  class Meta:
+    model = Comment
+    fields = ["content"]
 
-    def __init__(self, *args, **kwargs):
+  def __init__(self, *args, **kwargs):
         super(CommentForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.add_input(Submit('submit', 'Submit'))
